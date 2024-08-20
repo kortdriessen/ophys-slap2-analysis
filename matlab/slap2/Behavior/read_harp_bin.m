791d1275347886f6707c4a84058feaa56afe15a3
Lucas Kinsey <88734358+LKINSEY@users.noreply.github.com>
2024-08-20 11:34:39 -0700

    Made dedicated behavior folder - fixed read_harp_bin and made it more readable and included a data example

---
+++ matlab/slap2/Behavior/read_harp_bin.m
@@ -0,0 +1,109 @@
+function harpData = read_harp_bin(filePath, loadcell)
+%{
+ 21 byte width
+loadCellData.' =
+3   message type (1 byte)    
+26  length (1 byte)  
+33  address (1 byte)
+255 port (1 byte)  
+146 payload type (1 byte) 
+
+depending if timestamp bit is set (here it is)
+
+220   seconds (4 bytes) - uint32
+207   -
+3     -
+0     -
+165   microseconds (2 bytes) - uint 16
+61    -
+127   data (n-bytes dependent on payloadType)
+47    -
+137   -
+122   -
+139   -
+122   -
+115   -
+122   - 4 load cell pins, each 2 bytes
+0     
+0     
+0     - other data not used on register for this example
+0     
+0     
+0     
+0
+0
+35     checksum to verify integrity (continuously increasing as more dat is passed through read
+
+   Inputs:
+        filePath (string) : path to register
+        loadcell (boolean): true if handling loadcell register (Loadcells.harp only)
+   Outputs:
+        harpData (table) : Matlab table of data on register (if data exists, else NaN)
+%}
+
+fileID = fopen(filePath, 'rb'); 
+data = fread(fileID,  '*ubit8');
+if data(1) == 3
+    disp('Reading Event Data From Register')
+else
+    disp('Warning: You are not reading HARP events')
+end
+secondsPerTick = 32e-6;    
+keySets = [1, 2, 4, 8, 129, 130, 132, 136, 68];
+valueSets = {'uint8', 'uint16', 'uint32', 'uint64', 'int8', 'int16', 'int32', 'int64', 'single'};
+payloadTypes = containers.Map(keySets, valueSets);
+customValues = [1 2 4 8 1 2 4 8 4];
+payloadSizes = containers.Map(valueSets, customValues);
+
+deviceType = data(4); %points to what register data is on, if 0xFF, its a standalone device, ex: LoadCell device
+
+%breaking down for easier readability
+payloadInfo = dec2bin(data(5));
+isSigned = payloadInfo(1);
+isFloat = (payloadInfo(2));
+hasTimeStamp = (payloadInfo(4));
+payloadIndicator = (payloadInfo(5:end));
+
+payloadType = payloadTypes(  bitand(bin2dec([isSigned isFloat '0' hasTimeStamp payloadIndicator]),bitcmp(16, 'uint8'))  );
+elementSize = payloadSizes(payloadType); %retrieving size (in bytes) of payload1 element of the payload
+useTimeStamps = logical(hasTimeStamp);
+data = reshape(data, stride, []).';
+payloadSize = stride - 12;
+
+
+if useTimeStamps == true
+    disp('seconds bytes (4) + microseconds (2) + data ')
+    payloadData = data(:, 6:(6+1+payloadSize)); %ignoring headers
+    %Seconds are 4 bytes
+    secondsPayload = payloadSizes('uint32');
+    secondsSlice =  payloadData(:,1:(secondsPayload)); %seconds starts at 6th byte in stride, and it 4 bytes long
+    seconds = arrayfun(@(i) typecast(secondsSlice(i,:), 'uint32'), 1:size(secondsSlice,1), 'UniformOutput',false);
+    seconds = cell2mat(seconds);
+    
+    %Microseconds are 2 bytes
+    ticksPayload = payloadSizes('uint16');
+    ticksSlice = payloadData(:, secondsPayload+1:secondsPayload+ticksPayload);
+    ticks = arrayfun(@(i) typecast(ticksSlice(i,:), 'uint16'), 1:size(ticksSlice,1), 'UniformOutput',false);
+    ticks = cell2mat(ticks);
+    
+    seconds = double(double(ticks)*10e-6) + double(seconds);
+    
+    dataValuesSlice = payloadData(:, (secondsPayload + ticksPayload+1 ):payloadSize);
+    dataValues =  arrayfun(@(i) typecast(dataValuesSlice(i,:), payloadType), 1:size(payloadData,1), 'UniformOutput', false);
+    pinsRead = numel(dataValues{1});
+    dataValues = reshape(cell2mat(dataValues), 5, []).';
+
+    harpData = table(seconds.', dataValues, 'VariableNames', {'Seconds', 'Values'});
+else
+    disp('just data')
+    payloadData = data(:, 6:(6+payloadSize));
+
+    dataValuesSlice = payloadData(:, 1:payloadSize);
+    dataValues =  arrayfun(@(i) typecast(dataValuesSlice(i,:), payloadType), 1:size(payloadData,1), 'UniformOutput', false);
+    pinsRead = numel(dataValues{1});
+    dataValues = reshape(cell2mat(dataValues), 5, []).';
+    sequenceOfValues = 1:size(dataValues,1);
+    harpData = table(sequenceOfValues.', dataValues, 'VariableNames', {'Sequence', 'Values'});
+end
+
+end
---
+++ matlab/slap2/Behavior/sanityCheck.m
@@ -0,0 +1,44 @@
+testMouse = 'Y:\ophys\BCI\inactive_Mice\733585_SBCI24\Behavior\BCI'; mousePath = ls(testMouse);
+epochPaths = arrayfun(@(i) [testMouse filesep mousePath(i,:)], 1:size(mousePath,1), 'UniformOutput',false);
+epochTypes = {'moveOnly', 'BCIon   '};
+temp1 = cellfun(@(x) (split(x,'_')), epochPaths, 'UniformOutput',false);
+goodPaths = (cellfun(@(x) (x{end}), temp1, 'UniformOutput',false));
+[~,moveOnlyLogical] = ismember(goodPaths, epochTypes{1});
+[~,BCIlogical] = ismember(goodPaths, epochTypes{2});
+epochs = horzcat(epochPaths(logical(moveOnlyLogical))', epochPaths(logical(BCIlogical))');
+%sessionNum = epochs(1,:)
+registers = {
+    '\Behavior.harp\Register__PwmFrequencyDO1.bin', ...
+    '\Behavior.harp\Register__OutputSet.bin',...
+    '\Behavior.harp\Register__DigitalInputState.bin',...
+    '\Behavior.harp\Register__AnalogData.bin',...
+    '\Operation\SpoutPosition.csv',...
+    '\LoadCells.harp\Register__LoadCellData.bin'
+    }; %registers subject to change based on updates
+
+startPin = 0x1; stopPin = 0x10; %Subject to change based on bonsai updates
+
+for seshNum = 1:size(epochs,1)
+    disp('---------------------------top')
+    tic;
+    [moveOnly, BCIon] = epochs{seshNum,:};
+    BCIon = BCIon(find(~isspace(BCIon)));
+    
+    %MoveOnlys
+    moveOnly_audio = read_harp_bin([moveOnly registers{1}], false);
+    moveOnly_handshake = read_harp_bin([moveOnly registers{2}], false);
+    moveOnly_licks = read_harp_bin([moveOnly registers{3}], false);
+    moveOnly_adc = read_harp_bin([moveOnly registers{4}], false); moveOnly_adc.Values = moveOnly_adc.Values(:,1);
+    moveOnly_motor = readtable([moveOnly registers{5}]);
+    moveOnly_loadCell = read_harp_bin([moveOnly registers{6}], true);  
+    
+    %BCIs
+    BCI_audio = read_harp_bin([BCIon registers{1}], false);
+    BCI_handshake = read_harp_bin([BCIon registers{2}], false);
+    BCI_licks = read_harp_bin([BCIon registers{3}], false);
+    BCI_adc = read_harp_bin([BCIon registers{4}], false); moveOnly_adc.Values = moveOnly_adc.Values(:,1);
+    BCI_motor = readtable([BCIon registers{5}]);
+    BCI_loadCell = read_harp_bin([BCIon registers{6}], true);  
+    toc
+    disp('---------------------------bot (approx 30 seconds')
+end
