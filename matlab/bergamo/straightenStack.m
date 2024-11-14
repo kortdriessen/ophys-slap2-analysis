67f4e2052c691de1baffd1cf808e3abf59edb98e
Kaspar Podgorski <kaspar.podgorski@alleninstitute.org>
2024-11-14 15:38:01 -0800

    Updates to alignBergamoStack pipeline

--- matlab/bergamo/straightenStack.m
+++ matlab/bergamo/straightenStack.m
@@ -1,4 +1,4 @@
-function [IMout, IMc, IMsk]= straightenStack(IM, IMc, IMsk)
+function [IMout]= straightenStack(IM, IMc, IMsk)
 clipEdge = 40;
 maxShift = 16;
 scalingFac = round(size(IM,1)/size(IMc,1));
@@ -12,11 +12,11 @@ for Z1 = 1:(size(IM, 4)-1)
     output = dftregistration_clipped(Z1fft,Z2fft,4,maxShift);
     dX = dX + output(4); dY = dY+output(3);
     IMout(:,:,:,Z1+1) = imtranslate(IM(:,:,:,Z1+1), [dX dY]);
-    if nargout>1
-        IMc(:,:,:,Z1+1) = imtranslate(IMc(:,:,:,Z1+1), [dX dY]/scalingFac);
-    end
-    if nargout>2
-        IMsk(:,:,:,Z1+1) = imtranslate(IMsk(:,:,:,Z1+1), [dX dY]/scalingFac);
-    end
+    % if nargout>1
+    %     IMc(:,:,:,Z1+1) = imtranslate(IMc(:,:,:,Z1+1), [dX dY]/scalingFac);
+    % end
+    % if nargout>2
+    %     IMsk(:,:,:,Z1+1) = imtranslate(IMsk(:,:,:,Z1+1), [dX dY]/scalingFac);
+    % end
 end
 end
