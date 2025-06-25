dbecddc1e508ad7c19d9d58a2bb94e48ffa23df5
KasparP <k.podgorski@gmail.com>
2025-06-25 16:07:38 -0700

    starting to work on per-pixel uncertainties

--- matlab/preprocessing/multiRoiRegSLAP2.m
+++ matlab/preprocessing/multiRoiRegSLAP2.m
@@ -97,7 +97,6 @@ linerateHz = 1/meta.linePeriod_s;
 dt = linerateHz/aData.alignHz;
 numChannels = S2data.numChannels;
 
-
 if params.isReVolt
     numChannels = 1;
     redChannel =2;
@@ -177,6 +176,13 @@ trimCols = find(~all(isnan(Y(:,:,1)),1), 1, 'first'):find(~all(isnan(Y(:,:,1)),1
 Y = Y(trimRows, trimCols,:);
 sz = size(Y);
 
+%keep track of how many samples are averaged together to make images; this
+%is relevant for noise statistics
+nVisitsPerCycle = getVisitsPerCycle(meta);
+nVisitsPerCycle = nVisitsPerCycle(trimRows,trimCols);
+assert(max(nVisitsPerCycle,[],'all')<256);
+nVisitsPerCycle = single(nVisitsPerCycle);
+
 R = ones(nInitFrames);
 motion = zeros(2,nInitFrames,nInitFrames);
 for f1 = 1:nInitFrames
@@ -189,6 +195,7 @@ end
 [bestR, maxind] = max(median(R));
 frameInds = find(R(:,maxind)>=bestR);
 
+assert(aData.maxshift==round(aData.maxshift), 'params.maxshift must be an integer');
 [viewR, viewC] = ndgrid((1:(sz(1)+2*aData.maxshift))-aData.maxshift, (1:(sz(2)+2*aData.maxshift))-aData.maxshift); %view matrices for interpolation
 template = nan(2*aData.maxshift+sz(1), 2*aData.maxshift+sz(2));
 for fix = 1:length(frameInds)
@@ -276,6 +283,10 @@ try
         aErrorDS(DSframeIx) = 1-corrCoeff^2;
 
         A1 = interp2(1:sz(2), 1:sz(1), M1,viewC+motionDSc(DSframeIx), viewR+motionDSr(DSframeIx), 'linear', nan);
+        
+        %compute uncertainty of each pixel
+        %[A1,N1] = interpFrame(M1, viewC+motionDSc(DSframeIx), viewR+motionDSr(DSframeIx), nVisitsPerCycle);
+
         fTIF.WriteIMG(single(A1));
         if numChannels==2
             A2 = interp2(1:sz(2), 1:sz(1), M2,viewC+motionDSc(DSframeIx), viewR+motionDSr(DSframeIx), 'linear', nan);
