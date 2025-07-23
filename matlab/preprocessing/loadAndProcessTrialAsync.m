44342cdc47413d1199368acddded6212549bb406
KasparP <k.podgorski@gmail.com>
2025-07-23 09:18:35 -0700

    nonmax suppression in activity images
    generates *much* sharper estimates of synapse locations and can extract more densely spaces synapses in initial ad-hoc testing

--- matlab/preprocessing/loadAndProcessTrialAsync.m
+++ matlab/preprocessing/loadAndProcessTrialAsync.m
@@ -22,15 +22,23 @@ function  [IM, meanIM, IMc, aData, peaks, discardFrames]= loadAndProcessTrialAsy
     tmp = -tmp./(min(-0.005, prctile(tmp,5))); %normalize to the median-to-5th prctile interval, or 0.5% of intensity, whichever is larger
     thresh = params.motionThresh; %decrease thresh to be more stringent on motion correction
     window = 2*ceil(0.1*params.alignHz)+1;% a window in time to censor aronud motion events, ~0.2 seconds;
-    discardFrames = imclose(imdilate(tmp>thresh, ones(window,1)) | (tmp>(thresh/2) & imdilate(tmp>thresh, ones(2*window+1,1))), ones(window,1));
+    discardFrames = imclose(imdilate(tmp>thresh, ones(window,1)) & (tmp>(thresh/2)), ones(window,1));
     discardFrames(1:nInitFrames) = true;
     IM(:,:,discardFrames) = nan;
-    
+    if isfield(aData, 'varFacDS')
+        vIM = aData.varFacDS;
+        aData = rmfield(aData,'varFacDS');
+    else
+        vIM = [];
+    end
+
     try
-        [IMc, peaks] = localizeSourcesSLAP2(IM, [], params);
+        %[IMc, peaks] = localizeSourcesSLAP2(IM, [], params);
+       [IMc, peaks] = localizeSources_vIM(IM, vIM, params);
     catch ME
         IM([1 end],:,:) = nan;
         IM(:,[1 end],:) = nan;
-        [IMc, peaks] = localizeSourcesSLAP2(IM, [], params);
+        %[IMc, peaks] = localizeSourcesSLAP2(IM, vIM, params);
+        [IMc, peaks] = localizeSources_vIM(IM, vIM, params);
     end
 end
